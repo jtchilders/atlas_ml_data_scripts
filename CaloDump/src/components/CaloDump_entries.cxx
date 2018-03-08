03f0af0d09f9262eeb7321c85506c8948f007905
jtchilders <taylor@jtchilders.com>
2018-03-08 09:35:02 -0600

    removing CaloDump, next step copy full Athena Analysis package back in

--- CaloDump/src/components/CaloDump_entries.cxx
+++
@@ -1,6 +0,0 @@
-#include "CaloDump/CaloDump.h"
-#include "GaudiKernel/DeclareFactoryEntries.h"
-DECLARE_ALGORITHM_FACTORY( MyAlg  )
-DECLARE_FACTORY_ENTRIES( CaloDump ) {
-DECLARE_ALGORITHM( MyAlg )
-}
--- CaloDump/src/components/CaloDump_load.cxx
+++
@@ -1,2 +0,0 @@
-#include "GaudiKernel/LoadFactoryEntries.h"
-LOAD_FACTORY_ENTRIES( CaloDump )
